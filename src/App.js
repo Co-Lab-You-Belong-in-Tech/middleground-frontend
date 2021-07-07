fd1d052004e02835a2e4cf253becfbd73c51f1ea
Goku-kun <jethvadharmarajsinh@gmail.com>
2021-07-07 18:49:17 +0530

    ADD: hi-fi resources, UPDATE: Header to hi-fi design

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import Searchbar from "./Components/SearchBar";
 import Results from "./Components/Results";
 import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Logo from "./resources/Header-Logo.png";
 
 function App() {
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
   return (
     <div className="App">
+      <div class="header">
+        <div class="header-main">
+          <a href="https://middleground.netlify.app">
+            <img src={Logo} alt="Middle Ground logo" />
+            <h1>Middleground</h1>
+          </a>
+          <p>Your Source for a Balanced News Diet</p>
+        </div>
+        <p className="about-p">About</p>
+      </div>
       <Searchbar setResults={setResults} setLoading={setLoading} />
       {loading ? (
         <div className="loader">
